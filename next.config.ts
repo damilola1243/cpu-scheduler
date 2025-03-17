7de3e043ce39f80da1baa624c8f853f942267195
Damilola Ogun <lola@Mac.lan>
2025-03-16 22:19:02 -0500

    Fix: Added basePath and assetPrefix to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/cpu-scheduler', // Replace with your repository name if different
+  assetPrefix: '/cpu-scheduler/', // Replace with your repository name if different
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 export default nextConfig;
