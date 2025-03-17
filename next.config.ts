b43102e5ea92685066ef3612540e739eb67e1d84
Damilola Ogun <lola@Mac.lan>
2025-03-16 21:54:44 -0500

    Save local changes before rebase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/cpu-scheduler', // Required for GitHub Pages
+  assetPrefix: '/cpu-scheduler/', // Ensures static assets load correctly
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 export default nextConfig;
